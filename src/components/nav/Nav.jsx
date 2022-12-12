8a7e8bff880cffa1cbc85d57e732a94f4d6fd20d
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2022-12-12 22:35:00 +0900

    nav bar complete

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,53 @@
 import React from "react";
 import "./nav.css";
+import { HiHome } from "react-icons/hi";
+import { BiUserCircle } from "react-icons/bi";
+import { BiBook } from "react-icons/bi";
+import { FaHandshake } from "react-icons/fa";
+import { FaMailBulk } from "react-icons/fa";
+import { useState } from "react";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <HiHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <BiUserCircle />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BiBook />
+      </a>
+      <a
+        href="#services"
+        onClick={() => setActiveNav("#services")}
+        className={activeNav === "#services" ? "active" : ""}
+      >
+        <FaHandshake />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <FaMailBulk />
+      </a>
+    </nav>
+  );
 };
 
 export default Nav;
