2045212093766d896109c43f7f4dea18de66cd17
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2022-12-13 13:45:28 +0900

    Portfolio building

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./portfolio.css";
+import PONGIMG from "../../assets/PongGame.jpg";
+import RENTARETROIMG from "../../assets/rentaretro.jpg";
+import SUBSTRACKEDIMG from "../../assets/substracked.jpg";
+import TODOLISTIMG from "../../assets/ToDo_List.jpg";
+
+const data = [
+  { id: 1, image: PONGIMG, title: "Pong Clone", github: "", demo: "" },
+];
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5>My Works</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={PONGIMG} alt="tech-doc-img" />
+          </div>
+          <h3>Portfolio Item</h3>
+          <div className="portfolio__item-cta">
+            <a href="" className="btn" target={"_blank"}>
+              Github
+            </a>
+            <a href="" className="btn btn-primary" target={"_blank"}>
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
