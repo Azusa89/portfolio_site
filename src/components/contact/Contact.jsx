9325750f4ae2f102045d6682229b3f2942a4b54d
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2022-12-13 15:38:48 +0900

    contact section finnished

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,79 @@
 import React from "react";
 import "./contact.css";
+import { MdEmail } from "react-icons/md";
+import { AiFillPhone } from "react-icons/ai";
+import { FaMobileAlt } from "react-icons/fa";
+import { useRef } from "react";
+import emailjs from "emailjs-com";
 
-const contact = () => {
-  return <section id="contact">contact</section>;
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm(
+      "service_3vnf5ir",
+      "template_06uymay",
+      form.current,
+      "CVCj6LVv0lO6HJPcy"
+    );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get Tn Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>Chengy1989@hotmail.com</h5>
+            <a href="mailto:Chengy1989@hotmail.com" target="_blank">
+              Shoot me an Email!
+            </a>
+          </article>
+          <article className="contact__option">
+            <AiFillPhone className="contact__option-icon" />
+            <h4>Phone</h4>
+            <h5>N/A</h5>
+            <a href="mailto:Chengy1989@hotmail.com" target="_blank">
+              Ring me at work
+            </a>
+          </article>
+          <article className="contact__option">
+            <FaMobileAlt className="contact__option-icon" />
+            <h4>Mobile</h4>
+            <h5>N/A</h5>
+            <a href="mailto:Chengy1989@hotmail.com" target="_blank">
+              Ring me out of work
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="text" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
-export default contact;
+export default Contact;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,12 +1,39 @@
 import React from "react";
 import "./portfolio.css";
 import PONGIMG from "../../assets/PongGame.jpg";
+import TODOLISTIMG from "../../assets/ToDo_List.jpg";
 import RENTARETROIMG from "../../assets/rentaretro.jpg";
 import SUBSTRACKEDIMG from "../../assets/substracked.jpg";
-import TODOLISTIMG from "../../assets/ToDo_List.jpg";
 
 const data = [
-  { id: 1, image: PONGIMG, title: "Pong Clone", github: "", demo: "" },
+  {
+    id: 1,
+    image: PONGIMG,
+    title: "Pong Clone",
+    github: "https://github.com/Azusa89/PongClone",
+    demo: "../../pongDemo/pongindex.html",
+  },
+  {
+    id: 2,
+    image: TODOLISTIMG,
+    title: "ToDo List",
+    github: "https://github.com/Azusa89/ToDoList",
+    demo: "../../ToDoList/index.html",
+  },
+  {
+    id: 3,
+    image: RENTARETROIMG,
+    title: "Rent-a-Retro",
+    github: "https://github.com/clementckmc/Rent-a-retro",
+    demo: "https://rent-a-retro.herokuapp.com/",
+  },
+  {
+    id: 4,
+    image: SUBSTRACKEDIMG,
+    title: "Substracked",
+    github: "https://github.com/a-vasse/substracked",
+    demo: "https://www.substracked.com/",
+  },
 ];
 
 const Portfolio = () => {
@@ -14,21 +41,26 @@ const Portfolio = () => {
     <section id="portfolio">
       <h5>My Works</h5>
       <h2>Portfolio</h2>
+
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={PONGIMG} alt="tech-doc-img" />
-          </div>
-          <h3>Portfolio Item</h3>
-          <div className="portfolio__item-cta">
-            <a href="" className="btn" target={"_blank"}>
-              Github
-            </a>
-            <a href="" className="btn btn-primary" target={"_blank"}>
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article className="portfolio__item" key={id}>
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target={"_blank"}>
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target={"_blank"}>
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
   );
