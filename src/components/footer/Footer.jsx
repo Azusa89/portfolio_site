101c3c5b6a98d254e681df7e6f6541fc7944d99f
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2022-12-13 16:22:39 +0900

    first version done!

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./footer.css";
+import { AiFillFacebook } from "react-icons/ai";
+import { AiFillTwitterSquare } from "react-icons/ai";
+import { AiFillInstagram } from "react-icons/ai";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Kaho Cheng
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href="https://www.facebook.com/">
+          <AiFillFacebook />
+        </a>
+        <a href="https://www.instagram.com/">
+          <AiFillInstagram />
+        </a>
+        <a href="https://www.twitter.com/">
+          <AiFillTwitterSquare />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Kaho Cheng. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
