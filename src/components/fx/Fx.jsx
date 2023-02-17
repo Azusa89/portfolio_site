8ed84067b5bf10ba09e9b3e18069db1326b1580d
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2023-02-17 11:53:30 +0900

    blob function

--- src/components/fx/Fx.jsx
+++ src/components/fx/Fx.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import "./fx.css";
 
+const pointer = document.getElementById("pointer");
+document.body.onpointermove = (event) => {
+  const { clientX, clientY } = event;
+
+  pointer.style.left = `${clientX}px`;
+  pointer.style.top = `${clientY}px`;
+};
+
 function Fx() {
   return (
     <>
