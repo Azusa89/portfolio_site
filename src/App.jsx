b636fe80baa0950bc4095aae3410936d60bf8f72
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2023-02-17 11:30:48 +0900

    pointer fx test

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
+import Fx from "./components/fx/Fx";
 import Footer from "./components/footer/Footer";
+
 import "animate.css/animate.min.css";
 
 const app = () => {
   return (
     <>
+      <Fx />
       <Header />
       <Nav />
       <About />
---
+++ src/components/fx/Fx.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Fx() {
+  return <div className="pointer"></div>;
+}
+
+export default Fx;
