acd25aace81f227bbe9dbb1e6de5cc622eaacf05
Azusa89 <59798103+Azusa89@users.noreply.github.com>
2023-02-17 12:08:17 +0900

    logic

--- src/components/fx/Fx.jsx
+++ src/components/fx/Fx.jsx
@@ -1,18 +1,28 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import "./fx.css";
 
-const pointer = document.getElementById("pointer");
-document.body.onpointermove = (event) => {
-  const { clientX, clientY } = event;
-
-  pointer.style.left = `${clientX}px`;
-  pointer.style.top = `${clientY}px`;
-};
-
 function Fx() {
+  const [mousePos, setMousePos] = useState({});
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setMousePos({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <>
-      <div className="pointer"></div>
+      <div
+        className="pointer"
+        style={{ left: `${mousePos.x}px`, top: `${mousePos.y}px` }}
+      ></div>
       <div className="blur"></div>
     </>
   );
